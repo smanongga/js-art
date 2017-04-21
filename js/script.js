263440119291648bc6cb891def3201911d49df34
S. Manongga <smanongga@gmail.com>
2017-04-21 16:22:23 +1200

    Final js script

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
-var myCanvas = document.getElementById("myCanvas");
-var canvasContext = myCanvas.getContext('2d');
+var myCanvas;
+
+window.onload = loadCanvas;
+
+  function loadCanvas() {
+  myCanvas = document.getElementById("myCanvas");
+  canvasContext = myCanvas.getContext("2d");
+  setIntervalTime();
+
+ }
+  function squareBox() {
+    for (var i = 0; i <=400; i+=100) {
+      for(var k=0; k <=400; k+=100) {
+          canvasContext.fillStyle=getRandomColor();
+          canvasContext.fillRect(i,k,100,100);
+      }
+    }
+ }
+
+ function getRandomColor() {
+     var letters = '0123456789ABCDEF';
+     var color = '#';
+     for (var i = 0; i < 6; i++ ) {
+         color += letters[Math.floor(Math.random() * 16)];
+     }
+     return color;
+ }
+
+ function setIntervalTime() {
+      squareBox();
+      setTimeout(function(){
+        setIntervalTime();},1000
+      );
+ }
